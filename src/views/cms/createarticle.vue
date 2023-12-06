57ae7ec79a48d924909886f42f9f18d07fa62d96
xueyangspring@163.com <xueyangspring@163.com>
2023-12-06 22:13:55 +0800

    fix

--- src/views/cms/createarticle.vue
+++ src/views/cms/createarticle.vue
@@ -122,7 +122,7 @@ export default {
 
                 var url = 'https://app.xiyouqingsu.com/content/media/auth/getTencentOssToken?bucket=' + Bucket + "&uid=" + global.uid + "&token=" + global.token; // url 替换成您自己的后端服务
                 var xhr = new XMLHttpRequest();
-                xhr.withCredentials=true;
+                xhr.withCredentials=false;
                 xhr.open('GET', url, true);
                 //Access-Control-Allow-Origin
                 
